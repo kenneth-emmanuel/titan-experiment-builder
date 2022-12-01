aff55f9138707b43f04b46be697032482b49fb90
kenneth-emmanuel <kenneth.emmanuel@dev.superstruct.tech>
2022-12-02 04:51:11 +0530

    feat: add Titan logo to top of Sign-in page

--- titan-experiment-builder/src/pages/SignIn/SignIn.js
+++ titan-experiment-builder/src/pages/SignIn/SignIn.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import './SignIn.css'
+import titanLogo from '../../assets/images/titan_logo.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
@@ -25,35 +26,39 @@ const SignIn = () => {
     signInWithPopup(auth, provider).then((result) => {
       const credential = GoogleAuthProvider.credentialFromResult(result)
       const token = credential.accessToken
-      const user = result.user;
+      const user = result.user
       sessionStorage.setItem('auth_token', token)
       sessionStorage.setItem('logged_in_user', user.email)
-      console.log({user});
       history.push('/search')
     })
   }
 
   return (
-    <div className='signinContainer'>
-      <h1 className='heading2'>Sign in to get started</h1>
-      <button className='btn btn-primary' onClick={signInWithGoogle}>
-        <span className='faIconPosition'>
-          <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
-        </span>
+    <>
+      <div className='logoContainer'>
+        <img src={titanLogo} alt='' className='companyLogo mx-auto' />
+      </div>
+      <div className='signinContainer'>
+        <h1 className='heading2 break'>Sign in to get started</h1>
+        <button className='btn btn-primary break' onClick={signInWithGoogle}>
+          <span className='faIconPosition'>
+            <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+          </span>
 
-        <span>Sign in with Google</span>
-      </button>
-      <button className='btn btn-primary'>
-        <span className='faIconPosition'>
-          <FontAwesomeIcon icon={faMicrosoft}></FontAwesomeIcon>
-        </span>
-        <span>Sign in with Microsoft</span>
-      </button>
-      <p className='body2'>
-        <span>Terms of use</span> <span className='mx-3'>|</span>{' '}
-        <span>Privacy Policy</span>
-      </p>
-    </div>
+          <span>Sign in with Google</span>
+        </button>
+        <button className='btn btn-primary break'>
+          <span className='faIconPosition'>
+            <FontAwesomeIcon icon={faMicrosoft}></FontAwesomeIcon>
+          </span>
+          <span>Sign in with Microsoft</span>
+        </button>
+        <p className='body2 break'>
+          <span>Terms of use</span> <span className='mx-3'>|</span>{' '}
+          <span>Privacy Policy</span>
+        </p>
+      </div>
+    </>
   )
 }
 
