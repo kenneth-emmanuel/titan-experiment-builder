c0809a95db375c4daef1c437eafd129cc6a4551c
kenneth-emmanuel <kenneth.emmanuel@dev.superstruct.tech>
2022-12-02 01:33:53 +0530

    feat: create sign in page with custom sign-in buttons

--- titan-experiment-builder/src/index.js
+++ titan-experiment-builder/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- titan-experiment-builder/src/pages/SignIn/SignIn.js
+++ titan-experiment-builder/src/pages/SignIn/SignIn.js
@@ -1,29 +1,29 @@
+import './SignIn.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faMicrosoft } from '@fortawesome/free-brands-svg-icons'
+
 const SignIn = () => {
   return (
-    <div>
-      <div className='row'>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-        <div className='col-2'>
-          <p>SignIn</p>
-        </div>
-      </div>
+    <div className='signinContainer'>
+      <h1 className='heading2'>Sign in to get started</h1>
+      <button className='btn btn-primary'>
+        <span className='faIconPosition'>
+          <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+        </span>
+
+        <span>Sign in with Google</span>
+      </button>
+      <button className='btn btn-primary'>
+        <span className='faIconPosition'>
+          <FontAwesomeIcon icon={faMicrosoft}></FontAwesomeIcon>
+        </span>
+        <span>Sign in with Microsoft</span>
+      </button>
+      <p className='body2'>
+        <span>Terms of use</span> <span className='mx-3'>|</span>{' '}
+        <span>Privacy Policy</span>
+      </p>
     </div>
   )
 }
